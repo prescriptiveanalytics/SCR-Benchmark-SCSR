b320d287f7fd83b4ed57c93b8e09458b444abef1
Florian Bachinger <flo.bachinger@gmail.com>
2025-03-19 11:30:44 +0100

    add image boundary to dotplot, use different color schema

--- results/6-dotplot.py
+++ results/6-dotplot.py
@@ -23,23 +23,30 @@ results = pd.read_csv('./results/4-R2Score-and-Violations.csv')
 results['EquationName'] = [val.split('_')[0] for val in results["Instance"]]
 results['SampleSize'] = [ int(filename.split('_')[1]) for filename in results['Instance']]
 results['NoiseLevel'] = [ float(filename.split('_')[2].replace(',','.')) for filename in results['Instance']]
+results['ConstraintsDerivative0Count'] = results['ConstraintsCount'] - (results['ConstraintsDerivative1Count'] + results['ConstraintsDerivative2Count'])
+results['ConstraintsViolatedDerivative0Count'] = results['ConstraintsViolated'] - (results['ConstraintsViolatedDerivative1Count'] + results['ConstraintsViolatedDerivative2Count'])
 
 print(np.unique(results['EquationName']))
 print(len(np.unique(results['EquationName'])))
 
 results['ConstraintsCount'].fillna(1, inplace=True)
+results['ConstraintsDerivative0Count'].fillna(1, inplace=True)
 results['ConstraintsDerivative1Count'].fillna(1, inplace=True)
 results['ConstraintsDerivative2Count'].fillna(1, inplace=True)
 results['ConstraintsViolated'].fillna(1, inplace=True)
+results['ConstraintsViolatedDerivative0Count'].fillna(1, inplace=True)
 results['ConstraintsViolatedDerivative1Count'].fillna(1, inplace=True)
 results['ConstraintsViolatedDerivative2Count'].fillna(1, inplace=True)
 results['Test'].fillna(-10, inplace=True)
+results['R2_Test'] = results['Test']
 
 results['ConstraintsAchievedScaled'] = (1-(results['ConstraintsViolated']/results['ConstraintsCount']) ) * 100
+results['ConstraintsAchievedDerivative0Scaled'] = (1-((results['ConstraintsViolated']-results['ConstraintsViolatedDerivative1Count']-results['ConstraintsViolatedDerivative2Count'])/results['ConstraintsCount'])) * 100
 results['ConstraintsAchievedDerivative1Scaled'] = (1-(results['ConstraintsViolatedDerivative1Count']/results['ConstraintsDerivative1Count'])) * 100
 results['ConstraintsAchievedDerivative2Scaled'] = (1-(results['ConstraintsViolatedDerivative2Count']/results['ConstraintsDerivative2Count'])) * 100
 
 results['ConstraintsAchievedScaled'].fillna(0, inplace=True)
+results['ConstraintsAchievedDerivative0Scaled'].fillna(0, inplace=True)
 results['ConstraintsAchievedDerivative1Scaled'].fillna(0, inplace=True)
 results['ConstraintsAchievedDerivative2Scaled'].fillna(0, inplace=True)
 
@@ -49,7 +56,8 @@ results['EquationName'] = results['EquationName'].cat.set_categories(FEYNMAN_SRS
 
 
 print(results[['ConstraintsCount','ConstraintsViolated','ConstraintsAchievedScaled']])
-results = results[['EquationName','SampleSize','NoiseLevel', 'Test',"ConstraintsAchievedScaled","ConstraintsAchievedDerivative1Scaled","ConstraintsAchievedDerivative2Scaled" ]]
+results = results[['EquationName','SampleSize','NoiseLevel', 'R2_Test',"ConstraintsAchievedScaled","ConstraintsAchievedDerivative0Scaled","ConstraintsAchievedDerivative1Scaled","ConstraintsAchievedDerivative2Scaled" ]]
+
 results = results.groupby(
     ['EquationName','SampleSize','NoiseLevel' ]).median().reset_index()
 results.index = pd.RangeIndex(len(results.index))
@@ -63,13 +71,12 @@ for sampleSize in [1000,10000]:
 
   df = df.sort_values(['EquationName','SampleSize','NoiseLevel'])
   g = sns.PairGrid(df,
-                  x_vars=['Test',"ConstraintsAchievedScaled","ConstraintsAchievedDerivative1Scaled","ConstraintsAchievedDerivative2Scaled"], y_vars=["EquationName"],
+                  x_vars=['R2_Test',"ConstraintsAchievedScaled","ConstraintsAchievedDerivative0Scaled","ConstraintsAchievedDerivative1Scaled","ConstraintsAchievedDerivative2Scaled"], y_vars=["EquationName"],
                   height=8, aspect=.25, hue = 'NoiseLevel')
   plt.subplots_adjust(left=0.17, bottom=0.09, right=0.90, top=0.97, wspace=0.16, hspace=0.1)
 
   # Draw a dot plot using the stripplot function
-  g.map(sns.stripplot, size=7, orient="h", jitter=False,
-        palette="flare_r", linewidth=0, edgecolor=None, marker = '^', alpha = 0.5)
+  g.map(sns.stripplot, size=7, orient="h", jitter=False, hue = 'NoiseLevel', data = df, palette = 'tab10', marker = 'v', alpha = 0.5)
   g.add_legend()
 
   for ax in g.axes.flat:
@@ -77,14 +84,16 @@ for sampleSize in [1000,10000]:
       ax.xaxis.grid(False)
       ax.yaxis.grid(True)
       label = ax.get_xlabel()
-      if(label == "Test"):
+      if(label == "R2_Test"):
         ax.set( xlabel="mean $R^2$ validation", ylabel="")
       if(label == "ConstraintsAchievedScaled"):
         ax.set(xlim=(-10,110), xlabel="mean %\nachieved constraints\ntotal", ylabel="")
+      if(label == "ConstraintsAchievedDerivative0Scaled"):
+        ax.set(xlim=(-10,110), xlabel="mean %\nachieved constraints\nimage boundary", ylabel="")
       if(label == "ConstraintsAchievedDerivative1Scaled"):
-        ax.set(xlim=(-10,110), xlabel="mean %\nachieved constraints\ndegree 1", ylabel="")
+        ax.set(xlim=(-10,110), xlabel="mean %\nachieved constraints\nmonotonicity", ylabel="")
       if(label == "ConstraintsAchievedDerivative2Scaled"):
-        ax.set(xlim=(-10,110), xlabel="mean %\nachieved constraints\ndegree 2", ylabel="")
+        ax.set(xlim=(-10,110), xlabel="mean %\nachieved constraints\ncurvature", ylabel="")
 
 
   sns.despine(left=True, bottom=True)
